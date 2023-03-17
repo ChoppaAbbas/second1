900ed41f14e5423d05c0bfb16f35aadd4ad6acbc
Kamshat Kaliyeva <89693408+koppper@users.noreply.github.com>
2023-03-17 12:00:33 +0600

    debug true

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8vz0#11b5!18gk-(73hhn6arx0ywr!kexzhm!dk7g#cp&nll#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
